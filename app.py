3c4b3628aba6a24030e50e56a3d2a88d508493e4
Hiromu <tsongcun80@gmail.com>
2020-08-26 13:35:47 +0900

    大幅に変えました

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def register():
         password = request.form.get("password")
         conn = sqlite3.connect('himawari.db')
         c = conn.cursor()
-        c.execute("insert into users values(null,?,?)", (name,password))
+        c.execute("insert into user values(null,?,?)", (name,password))
         conn.commit()
         conn.close()
         return redirect('/login')
@@ -37,7 +37,7 @@ def login():
         password = request.form.get("password")
         conn = sqlite3.connect('himawari.db')
         c = conn.cursor()
-        c.execute("select id from users where name = ? and password = ?", (name, password) )
+        c.execute("select id from user where name = ? and password = ?", (name, password) )
         user_id = c.fetchone()
         conn.close()
         print(type(user_id))
@@ -49,7 +49,25 @@ def login():
 
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    if 'user_id' in session :
+        user_id = session['user_id']
+        conn = sqlite3.connect('himawari.db')
+        c = conn.cursor()
+        c.execute("select name from user where id = ?", (user_id,))
+        user_info = c.fetchone()
+#         # user_infoの中身を確認
+
+#         # 課題1の答えはここ del_flagが0のものだけ表示する
+#         # 課題2の答えはここ 保存されているtimeも表示する
+#         c.execute("select id,time from user where userid = ?  = 0 order by id", (user_id,))
+#         for row in c.fetchall():
+#             comment_list.append({"id": row[0], "time":row[1]})
+
+        c.close()
+        return render_template('index.html')
+    else:
+        return redirect("/login")
+
 
 @app.route("/hospital")
 def hospital():
